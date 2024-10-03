95bb5ed09a76241797e4d9ec321a6c0d89cff154
josefdaly <josefdaly@gmail.com>
2024-10-03 16:25:51 -0400

    Variety of Changes
    removes full tilemap rerender on every frame.
    adds music.

--- tilemap.py
+++ tilemap.py
@@ -2,34 +2,56 @@ import pygame
 from numpy import zeros, array, random
 
 
+class MapTile:
+    """
+    Abstraction for instantiated tile on map
+    """
+    def __init__(self, tile, rect):
+        self.tile = tile
+        self.rect = rect
+
+
 class Tilemap:
-    def __init__(self, tileset, size):
+    def __init__(self, game, tileset, size):
+        self.game = game
         self.size = size
         self.tileset = tileset
         self.map = zeros(size, dtype=int)
-        self.tile_rects = []
+        self.collision_rects = []
+        self.all_rects = []
+        self.tiles = {}
 
         h, w = self.size
-        self.image = pygame.Surface((16*w, 16*h))
+        self.image = pygame.Surface((self.game.TILE_WIDTH*w, self.game.TILE_HEIGHT*h))
         self.rect = self.image.get_rect()
 
     def create_and_render_room(self, collision_tile_map, convert_tile_reference=None):
         m, n = self.map.shape
-        tiles = []
+        self.tiles = {}
+        self.all_rects = []
+        self.collision_rects = []
         for i in range(m):
             for j in range(n):
                 tile_no = self.map[i, j]
                 if convert_tile_reference:
                     tile_no = convert_tile_reference(tile_no)
                 tile = self.tileset.tiles[tile_no]
+                tile_rect = tile.get_rect()
+                tile_rect.y = i*self.game.TILE_HEIGHT
+                tile_rect.x = j*self.game.TILE_WIDTH
+                self.tiles[(j*self.game.TILE_WIDTH, i*self.game.TILE_HEIGHT)] = MapTile(
+                    tile, tile_rect
+                )
+                self.all_rects.append(tile_rect)
                 if collision_tile_map[i, j] == 'X':
-                    tile_rect = tile.get_rect()
-                    tiles.append(tile_rect)
-                    tile_rect.y = i*16
-                    tile_rect.x = j*16
-                    tiles.append(tile_rect)
-                self.image.blit(tile, (j*16, i*16))
-        self.tile_rects = tiles
+                    self.collision_rects.append(tile_rect)
+                self.image.blit(tile, (j*self.game.TILE_WIDTH, i*self.game.TILE_HEIGHT))
+
+    def get_maptile_by_rect(self, rect):
+        return self.tiles[(rect.x, rect.y)]
+
+    def render_maptile(self, image, maptile):
+        image.blit(maptile.tile, (maptile.rect.x, maptile.rect.y))
 
     def set_room(self, room, collision_tile_map, convert_tile_reference=None):
         self.map = array(room)
--- zelda.py
+++ zelda.py
@@ -19,8 +19,7 @@ horizantal_flip_files = (
     'assets/link_left1.png',
     'assets/link_left2.png',
 )
-TILE_WIDTH = 16
-TILE_HEIGHT = 16
+overworld_music_file = 'assets/overworld.mp3'
 ROOM_WIDTH = 16
 ROOM_HEIGHT = 11
 FPS = 60
@@ -29,6 +28,10 @@ class Game:
     W = 256
     H = 176
     SIZE = W, H
+
+    TILE_WIDTH = 16
+    TILE_HEIGHT = 16
+
     ROOM_HEIGHT_PIXELS = ROOM_HEIGHT * TILE_HEIGHT
     ROOM_WIDTH_PIXELS = ROOM_WIDTH * TILE_WIDTH
     PLAYER_MOVEMENT_KEYS = (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)
@@ -49,14 +52,14 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode(self.SIZE, pygame.SCALED)
         pygame.display.set_caption("Zelda")
-        self.tileset = Tileset(overworld_tile_file, size=(TILE_HEIGHT, TILE_WIDTH))
-        self.tilemap = Tilemap(self.tileset, size=(ROOM_HEIGHT, ROOM_WIDTH))
+        self.tileset = Tileset(overworld_tile_file, size=(self.TILE_HEIGHT, self.TILE_WIDTH))
+        self.tilemap = Tilemap(self, self.tileset, size=(ROOM_HEIGHT, ROOM_WIDTH))
         self.next_tilemap = None
         self.next_tilemap_loc = None
         self.player = Player(
             player_files,
             horizantal_flip_files,
-            starting_loc=find_map_tile_location(117, ROOM_WIDTH, TILE_HEIGHT, TILE_WIDTH)
+            starting_loc=find_map_tile_location(117, ROOM_WIDTH, self.TILE_HEIGHT, self.TILE_WIDTH)
         )
         self.sprite_list = pygame.sprite.Group()
         self.sprite_list.add(self.player)
@@ -76,6 +79,7 @@ class Game:
             ROOM_HEIGHT,
             ROOM_WIDTH,
         )
+        pygame.mixer.music.load(overworld_music_file)
 
     def move_tilemap(self):
         if self.changing_rooms:
@@ -89,65 +93,76 @@ class Game:
             )
             self.player.move(self.tilemap_velocity)
 
+    def render_maptiles_to_screen(self, maptiles):
+        for maptile in maptiles:
+            self.tilemap.render_maptile(self.screen, maptile)
+
+    def handle_player_movement(self):
+        if not self.changing_rooms and self.player.should_be_moving(self.tilemap.collision_rects):
+            rects_to_render_indices = self.player.collision_rects(self.tilemap.all_rects) # collect all recs previous player location stood over
+            self.player.update_player_location()
+            self.render_maptiles_to_screen([
+                self.tilemap.get_maptile_by_rect(self.tilemap.all_rects[idx])
+                for idx in rects_to_render_indices
+            ])
+
+
+    def handle_room_change_state(self):
+        if self.player.is_walking_over_edge(self.ROOM_HEIGHT_PIXELS, self.ROOM_WIDTH_PIXELS):
+            if not self.changing_rooms:
+                self.tilemap_velocity = (self.player.dir[0] * -1, self.player.dir[1] * -1)
+                self.next_tilemap = Tilemap(self, self.tileset, size=(ROOM_HEIGHT, ROOM_WIDTH))
+                self.current_room = (
+                    self.current_room[0] + self.player.dir[1],
+                    self.current_room[1] + self.player.dir[0],
+                )
+                self.next_tilemap.set_room(
+                    self.overworld_rooms[self.current_room[0] * 16 + self.current_room[1]],
+                    self.overworld_rooms_collision_data[self.current_room[0] * 16 + self.current_room[1]],
+                    convert_tile_reference=hex_reference_to_integer,
+                )
+                self.next_tilemap_loc = (self.ROOM_WIDTH_PIXELS * self.player.dir[0], self.ROOM_HEIGHT_PIXELS * self.player.dir[1])
+            self.changing_rooms = True
+
+        if self.next_tilemap and self.changing_rooms == True:
+            if self.next_tilemap_loc == (0, 0):
+                self.player.move(self.player.velocity)
+                self.changing_rooms = False
+                self.tilemap = self.next_tilemap
+                self.tilemap_loc = (0, 0)
+                self.next_tilemap = None
+                self.next_tilemap_loc = None
+                self.tilemap_velocity = (0, 0)
+
     def run(self):
         self.tilemap.set_room(
             self.overworld_rooms[self.current_room[0] * 16 + self.current_room[1]],
             self.overworld_rooms_collision_data[self.current_room[0] * 16 + self.current_room[1]],
             convert_tile_reference=hex_reference_to_integer,
         )
+        self.screen.blit(self.tilemap.image, self.tilemap_loc)
+        pygame.mixer.music.play(-1)
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                 elif event.type == pygame.KEYDOWN:
                     if event.key in self.PLAYER_MOVEMENT_KEYS:
-                        dir = self.DIR_MAP[event.key]
-                        self.player.set_velocity(dir)
-                    if event.key == pygame.K_a:
-                        self.changing_rooms = True
-                        self.tilemap_velocity = (-1, 0)
+                        self.player.set_velocity(self.DIR_MAP[event.key])
                 elif event.type == pygame.KEYUP:
                     if event.key in self.PLAYER_MOVEMENT_KEYS:
-                        dir = self.DIR_MAP[event.key]
-                        self.player.stop(dir)
+                        self.player.stop(self.DIR_MAP[event.key])
 
-            if not self.changing_rooms and self.player.should_be_moving(self.tilemap.tile_rects):
-                self.player.update_player_location()
-
-            if self.player.is_walking_over_edge(self.ROOM_HEIGHT_PIXELS, self.ROOM_WIDTH_PIXELS):
-                if not self.changing_rooms:
-                    self.tilemap_velocity = (self.player.dir[0] * -1, self.player.dir[1] * -1)
-                    self.next_tilemap = Tilemap(self.tileset, size=(ROOM_HEIGHT, ROOM_WIDTH))
-                    self.current_room = (
-                        self.current_room[0] + self.player.dir[1],
-                        self.current_room[1] + self.player.dir[0],
-                    )
-                    self.next_tilemap.set_room(
-                        self.overworld_rooms[self.current_room[0] * 16 + self.current_room[1]],
-                        self.overworld_rooms_collision_data[self.current_room[0] * 16 + self.current_room[1]],
-                        convert_tile_reference=hex_reference_to_integer,
-                    )
-                    self.next_tilemap_loc = (self.ROOM_WIDTH_PIXELS * self.player.dir[0], self.ROOM_HEIGHT_PIXELS * self.player.dir[1])
-                self.changing_rooms = True
-
-            if self.next_tilemap and self.changing_rooms == True:
-                if self.next_tilemap_loc == (0, 0):
-                    self.player.move(self.player.velocity)
-                    self.changing_rooms = False
-                    self.tilemap = self.next_tilemap
-                    self.tilemap_loc = (0, 0)
-                    self.next_tilemap = None
-                    self.next_tilemap_loc = None
-                    self.tilemap_velocity = (0, 0)
-
+            self.handle_player_movement()
+            self.handle_room_change_state()
             self.move_tilemap()
             self.update_display()
             self.clock.tick(FPS)
         pygame.quit()
 
     def update_display(self):
-        self.screen.blit(self.tilemap.image, self.tilemap_loc)
         if self.next_tilemap:
+            self.screen.blit(self.tilemap.image, self.tilemap_loc)
             self.screen.blit(self.next_tilemap.image, self.next_tilemap_loc)
         self.sprite_list.draw(self.screen)
         pygame.display.update()
